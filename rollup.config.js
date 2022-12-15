2b7b297a292f2a2461abafdbfc9c880699f46e93
12Me21 <12Me21.mc@gmail.com>
2022-12-14 21:53:39 -0500

    rollp

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import {nodeResolve} from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+
+global.__defineGetter__('__filename', ()=>new URL(import.meta.url))
+import terser from '@rollup/plugin-terser'
+
+let f = new URL('.', import.meta.url)
+
+export default {
+	input: 'parse.js',
+	output: {
+		dir: 'rollup',
+		plugins: [
+		],
+	},
+	plugins: [
+		nodeResolve(),
+		commonjs(),
+		terser({
+			ecma: 3*673,
+			parse: {},
+			compress: {},
+			mangle: {},
+			format: {
+				semicolons: false,
+				comments: false,
+				max_line_len: 70,
+				quote_style: 3,
+				preamble: `${f}\nwhile(1)break ${f.protocol.slice(0,-1)}`,
+			},
+		}),
+	],
+}
