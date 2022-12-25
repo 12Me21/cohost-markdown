d34add3b804452bdcb29746a45549331887648dd
12Me21 <12Me21.mc@gmail.com>
2022-12-25 09:50:07 -0500

    correct handling of invalid :emote:s

--- cohost-emotes.js
+++ cohost-emotes.js
@@ -18,28 +18,31 @@ const EMOTES = {
 	'host-stare':  {url:'host-stare',cohost_plus:true},
 }
 
-const EMOTE_REGEX = /:(chunks|eggbug|sixty|unyeah|yeah):/g
-
-const PLUS_REGEX = /:(chunks|eggbug|sixty|unyeah|yeah|host-(?:aww|cry|evil|frown|joy|love|nervous|plead|shock|stare)):/g
+const EMOTE_REGEX = /:([-\w]+):/g
 
 export default function emote({hasCohostPlus}){
 	return node=>{
 		if (node.nodeType!=Node.TEXT_NODE)
 			return
 		
-		let parts = node.data.split(hasCohostPlus ? PLUS_REGEX : EMOTE_REGEX)
+		let parts = node.data.split(EMOTE_REGEX)
 		if (parts.length<=1)
 			return
 		let list = []
+		// TODO: currently, invalid emotes will create extra text nodes. this is probably fine, but the original code doesn't, so
 		for (let i=0; i<=parts.length-2; i+=2) {
 			let text = parts[i]
 			let name = parts[i+1]
 			let emote = EMOTES[name]
-			let img = document.createElement('img')
-			img.className = 'emote'
-			img.src = `emotes/${emote.url}.webp`
-			img.alt = `:${name}:`
-			list.push(text, img)
+			if (emote && (hasCohostPlus || !emote.cohost_plus)) {
+				let img = document.createElement('img')
+				img.className = 'emote'
+				img.src = `emotes/${emote.url}.webp` // yeah yeah we're using webp, oh well. The files were smaller, ok ?
+				img.alt = `:${name}:`
+				list.push(text, img)
+			} else {
+				list.push(text+":"+name+":")
+			}
 		}
 		list.push(parts[parts.length-1])
 		return list
